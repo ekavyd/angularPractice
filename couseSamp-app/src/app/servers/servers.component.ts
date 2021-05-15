b0c27473610c87b6941544ad76afb9be644622c4
LAPTOP-JS7PJA3E\Collin <Collin.d.reed@gmail.com>
2021-05-15 18:09:57 -0400

    last bit of basics review

--- couseSamp-app/src/app/servers/servers.component.ts
+++ couseSamp-app/src/app/servers/servers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule} from '@angular/forms'
+
 
 @Component({
   selector: 'app-servers',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test Server';
+  createrName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer=true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server Created! Name is '+ this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+
+  emptyCreater(){
+    this.createrName = '';
+
   }
 
 }
